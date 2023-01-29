b3fe2e6f578abdcd73f993f07af6019b1eac3116
viktor111 <swifrorlilko@gmail.com>
2023-01-29 21:41:39 +0200

    ADD: args parse server or client

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+use clap::Parser;
+
+mod server;
+mod client;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Name of the person to greet
+   #[arg(short, long, default_value_t)]
+   server: String,
+
+   #[arg(short, long, default_value_t)]
+    client: bool
+}
 
 #[tokio::main]
 async fn main() {
+    let args = Args::parse();
+    if args.server != ""  {
+        println!("Server running in {} directory...", args.server);
+    } else if args.client {
+        println!("Client running...");
+    }
 }
